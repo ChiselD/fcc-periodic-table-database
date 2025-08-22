17d2ded8385e947bc353c867d23fdf4e7a84a38a
Jinx <chisel.delahanty@gmail.com>
2025-08-22 09:00:26 -0400

    refactor: clean up variable names and add comments

--- element.sh
+++ element.sh
@@ -4,34 +4,43 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+GET_ELEMENT_INFO () {
+  echo "info about $1"
+}
+
 # check if user entered an argument
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  # check for element in 'elements' table
+  # check for element in 'elements' table by possible number
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    FIND_ELEMENT_RESULT=$($PSQL "
+    ELEMENT_NAME=$($PSQL "
       select name from elements
       where atomic_number=$1
     ")
-    if [[ -z $FIND_ELEMENT_RESULT ]]
+    # if not found
+    if [[ -z $ELEMENT_NAME ]]
     then
       echo "I could not find that element's number in the database."
+    # otherwise run 'get info' function
     else
-      echo "Element number found!"
+      GET_ELEMENT_INFO $ELEMENT_NAME
     fi
+  # check for element in 'elements' table by possible name or symbol
   else
-    FIND_ELEMENT_RESULT=$($PSQL "
+    ELEMENT_NAME=$($PSQL "
       select name from elements
       where symbol='$1' or name='$1'
     ")
-    if [[ -z $FIND_ELEMENT_RESULT ]]
+    # if not found
+    if [[ -z $ELEMENT_NAME ]]
     then
       echo "I could not find that element's name or symbol in the database."
+    # otherwise run 'get info' function
     else
-      echo "Element name or symbol found!"
+      GET_ELEMENT_INFO $ELEMENT_NAME
     fi
   fi
 fi
