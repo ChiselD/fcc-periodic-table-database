34f0343b83e5abc1365ac0e709b7dbbf1f96e88e
Jinx <chisel.delahanty@gmail.com>
2025-08-22 09:22:16 -0400

    feat: complete element query

--- element.sh
+++ element.sh
@@ -5,18 +5,14 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 GET_ELEMENT_INFO () {
-  echo "info about $1"
   ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where atomic_number=$1")
   EL_NAME=$($PSQL "select name from elements where atomic_number=$1")
   EL_SYMBOL=$($PSQL "select symbol from elements where atomic_number=$1")
-  EL_TYPE=$($PSQL "")
-  ATOMIC_MASS=$($PSQL "")
-  MELTING_POINT=$($PSQL "")
-  BOILING_POINT=$($PSQL "")
-  echo $ATOMIC_NUMBER
-  echo "The element with atomic number $ATOMIC_NUMBER is $EL_NAME ($EL_SYMBOL).
-    It's a nonmetal, with a mass of 1.008 amu.
-    Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  EL_TYPE=$($PSQL "select t.type from types t join properties p on t.type_id=p.type_id where atomic_number=$1")
+  ATOMIC_MASS=$($PSQL "select atomic_mass from properties where atomic_number=$1")
+  MELTING_POINT=$($PSQL "select melting_point_celsius from properties where atomic_number=$1")
+  BOILING_POINT=$($PSQL "select boiling_point_celsius from properties where atomic_number=$1")
+  echo "The element with atomic number $ATOMIC_NUMBER is $EL_NAME ($EL_SYMBOL). It's a $EL_TYPE, with a mass of $ATOMIC_MASS amu. $EL_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 }
 
 # check if user entered an argument
