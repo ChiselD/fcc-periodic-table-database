5f9a06fe29a6fb94b406b2a0acea7a0e712345da
Jinx <chisel.delahanty@gmail.com>
2025-08-22 08:49:26 -0400

    feat: complete element search functionality

--- element.sh
+++ element.sh
@@ -2,11 +2,36 @@
 
 # Script to get information about elements from the periodic table database
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+# check if user entered an argument
 if [[ -z $1 ]]
 then
-  echo Please provide an element as an argument.
+  echo "Please provide an element as an argument."
 else
-  echo Argument found!
+  # check for element in 'elements' table
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    FIND_ELEMENT_RESULT=$($PSQL "
+      select name from elements
+      where atomic_number=$1
+    ")
+    if [[ -z $FIND_ELEMENT_RESULT ]]
+    then
+      echo "I could not find that element's number in the database."
+    else
+      echo "Element number found!"
+    fi
+  else
+    FIND_ELEMENT_RESULT=$($PSQL "
+      select name from elements
+      where symbol='$1' or name='$1'
+    ")
+    if [[ -z $FIND_ELEMENT_RESULT ]]
+    then
+      echo "I could not find that element's name or symbol in the database."
+    else
+      echo "Element name or symbol found!"
+    fi
+  fi
 fi
